1a89bc9f7da562d03b00fcd7c1cfd19109c9e7f3
chels <s22120160@alumnos.itsur.edu.mx>
2025-10-14 20:58:10 -0600

    Implementación de las funciones Sell, Delete y Edit con Room y ViewModel.

--- app/src/main/java/com/example/inventory/ui/item/ItemDetailsViewModel.kt
+++ app/src/main/java/com/example/inventory/ui/item/ItemDetailsViewModel.kt
@@ -18,8 +18,8 @@ package com.example.inventory.ui.item
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import com.example.inventory.data.ItemsRepository
 import androidx.lifecycle.viewModelScope
+import com.example.inventory.data.ItemsRepository
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.filterNotNull
@@ -32,11 +32,15 @@ import kotlinx.coroutines.launch
  */
 class ItemDetailsViewModel(
     savedStateHandle: SavedStateHandle,
-    private val itemsRepository: ItemsRepository
+    private val itemsRepository: ItemsRepository,
 ) : ViewModel() {
 
     private val itemId: Int = checkNotNull(savedStateHandle[ItemDetailsDestination.itemIdArg])
 
+    /**
+     * Holds the item details ui state. The data is retrieved from [ItemsRepository] and mapped to
+     * the UI state.
+     */
     val uiState: StateFlow<ItemDetailsUiState> =
         itemsRepository.getItemStream(itemId)
             .filterNotNull()
@@ -47,6 +51,25 @@ class ItemDetailsViewModel(
                 started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
                 initialValue = ItemDetailsUiState()
             )
+
+    /**
+     * Reduces the item quantity by one and update the [ItemsRepository]'s data source.
+     */
+    fun reduceQuantityByOne() {
+        viewModelScope.launch {
+            val currentItem = uiState.value.itemDetails.toItem()
+            if (currentItem.quantity > 0) {
+                itemsRepository.updateItem(currentItem.copy(quantity = currentItem.quantity - 1))
+            }
+        }
+    }
+
+    /**
+     * Deletes the item from the [ItemsRepository]'s data source.
+     */
+    suspend fun deleteItem() {
+        itemsRepository.deleteItem(uiState.value.itemDetails.toItem())
+    }
 
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
